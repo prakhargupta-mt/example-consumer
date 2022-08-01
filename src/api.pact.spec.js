c8abf19c8d2c638b3d12cc630ab430bb27386f01
Prakhar Gupta <prakhar.gupta@mindtickle.com>
2022-08-01 13:38:46 +0530

    Update: Added price as expectation for pact

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -17,10 +17,10 @@ describe('API Pact test', () => {
 
     describe('retrieving a product', () => {
         test('ID 10 exists', async () => {
-            const expectedProduct = {id: '10', type: 'CREDIT_CARD', name: '28 Degrees'}
+            // const expectedProduct = {id: '10', type: 'CREDIT_CARD', name: '28 Degrees'}
 
             // New field price introduced
-            // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0}
+            const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0}
 
             await mockProvider.addInteraction({
                 state: 'a product with ID 10 exists',
